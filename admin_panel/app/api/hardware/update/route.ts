c2ec5d855ba2145ede78e65384cc2555d885a4e4
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-03-25 12:39:28 +0000

    update

--- admin_panel/app/api/hardware/update/route.ts
+++ admin_panel/app/api/hardware/update/route.ts
@@ -4,7 +4,7 @@ import { connection } from "@/lib/db";
 export async function POST(req: NextRequest) {
     const { conductor_id, password, latitude, longitude  } = await req.json()
     const location = `${latitude},${longitude}`
-    if (conductor_id && password && latitude && longitude) {
+    if (conductor_id && password && latitude && longitude && latitude != 0.000000 && longitude != 0.000000) {
         try {
             const sql = `UPDATE buses SET currentlocation = ${location} WHERE id = (SELECT busid FROM conductors WHERE id = ${conductor_id} AND password = ${password})`
             await connection.query(sql)
