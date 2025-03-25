104857b1ec9932a85e079771d8571e9551bc3315
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-03-25 13:12:21 +0000

    sql

--- admin_panel/app/api/hardware/update/route.ts
+++ admin_panel/app/api/hardware/update/route.ts
@@ -6,10 +6,12 @@ export async function POST(req: NextRequest) {
     const location = `${latitude},${longitude}`
     if (conductor_id && password && latitude && longitude && latitude != 0.000000 && longitude != 0.000000) {
         try {
-            const sql = `UPDATE buses SET currentlocation = ${location} WHERE id = (SELECT busid FROM conductors WHERE id = ${conductor_id} AND password = ${password})`
+            const sql = `UPDATE buses SET currentlocation = '${latitude},${longitude}' WHERE id = (SELECT busid FROM conductors WHERE id = '${conductor_id}' AND password = '${password}' LIMIT 1);`
+            console.log(sql)
             await connection.query(sql)
             return NextResponse.json({ success: true })
         } catch (e) {
+            console.error(e)
             return NextResponse.json({ success: false, message: e })
         }
     }
